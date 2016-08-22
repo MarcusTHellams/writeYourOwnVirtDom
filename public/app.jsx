f496b2973464ffc4d4b2f771b2ebf9f8e3e79586
Marcus <mhellams@hotmail.com>
2016-08-22 19:56:31 -0400

    Diffing props

--- public/app.jsx
+++ public/app.jsx
@@ -30,6 +30,11 @@ function updateElement($parent, newNode, oldNode, index = 0) {
 	} else if (changed(newNode, oldNode)) {
 		$parent.replaceChild(createElement(newNode), $parent.childNodes[index]);
 	} else if (newNode.type) {
+		updateProps(
+			$parent.childNodes[index],
+			newNode.props,
+			oldNode.props
+		)
 		const newLength = newNode.children.length;
 		const oldLength = oldNode.children.length;
 
@@ -64,6 +69,19 @@ function setProp($target, name, value) {
 
 }
 
+function removeProp($target, name, value) {
+	if (isCustomProp(name)) {
+		return;
+	} else if (name === 'className') {
+		$target.removeAttribute('class');
+	} else if (typeof value === 'boolean') {
+		removeBooleanProp($target, name, value)
+	} else {
+		$target.removeAttribute(name, value);
+	}
+
+}
+
 function isCustomProp(name) {
 	return false;
 }
@@ -77,9 +95,29 @@ function setBooleanProp($target, name, value) {
 	}
 }
 
+function removeBooleanProp($target, name) {
+	$target.removeAttribute(name);
+	$target[name] = false;
+}
+
 function setProps($target, props) {
 	Object.keys(props).forEach(name => {
 		setProp($target, name, props[name]);
+	});
+}
+
+function updateProp($target, name, newVal, oldVal) {
+	if (!newVal) {
+		removeProp($target, name, oldVal);
+	} else if (!oldVal || newVal !== oldVal) {
+		setProp($target, name, newVal);
+	}
+}
+
+function updateProps($target, newProps, oldProps = {}) {
+	const props = Object.assign({}, newProps, oldProps);
+	Object.keys(props).forEach(name => {
+		updateProp($target, name, newProps[name], oldProps[name]);
 	});
 }
 
@@ -97,14 +135,35 @@ const b = (
     <li>Hello</li>
   </ul>
 );
+
+const f = (
+  <ul style="list-style: none;">
+    <li className="item">item 1</li>
+    <li className="item">
+      <input type="checkbox" checked={true} />
+      <input type="text" disabled={false} />
+    </li>
+  </ul>
+);
+
+
+const g = (
+  <ul style="list-style: none;">
+    <li className="item item2">item 1</li>
+    <li style="background: red;">
+      <input type="checkbox" checked={false} />
+      <input type="text" disabled={true} />
+    </li>
+  </ul>
+);
 /* beautify preserve:end */
 
 console.log(a, b);
 const $root = document.getElementById('root');
 const $reload = document.getElementById('reload');
 
-updateElement($root, a);
+updateElement($root, f);
 
 $reload.addEventListener('click', () => {
-	updateElement($root, b, a);
+	updateElement($root, g, f);
 });
